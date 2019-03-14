26b2613d7fa72e0dae56265a089fcc65eb614766
Carlos Mejia <carlosmejia083@hotmail.com>
2019-03-14 11:25:09 -0500

    Initial Commit

---
+++ gamingnews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gamingnews
+//
+//  Created by Carlos Mejia on 3/14/19.
+//  Copyright © 2019 Carlos Mejia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
