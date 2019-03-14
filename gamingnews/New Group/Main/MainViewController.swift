c1a591686867bc0698f08545409cc0176fa75e5e
Carlos Mejia <luiscarlos083@hotmail.com>
2019-03-14 11:58:10 -0500

    Initial Project configuration added.

--- gamingnews/New Group/Main/MainViewController.swift
+++ gamingnews/New Group/Main/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  gamingnews
 //
 //  Created by Carlos Mejia on 3/14/19.
@@ -8,13 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = .red
     }
-
-
 }
-
---
+++ gamingnews/Shared/Config/AppDelegate.swift
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.swift
+//  gamingnews
+//
+//  Created by Carlos Mejia on 3/14/19.
+//  Copyright © 2019 Carlos Mejia. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        setupInitialVC()
+        
+        return true
+    }
+    
+    // MARK: - Private Methods
+    
+    /**
+     Method to create the main window and the initial view controller to show.
+    **/
+    private func setupInitialVC() {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = MainViewController()
+        window?.makeKeyAndVisible()
+    }
+}
